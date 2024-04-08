ebc2614e064670c389bc2a7a96c20ed1207d9b59
ShellyCS <mailstosaily@gmail.com>
2024-04-08 14:43:21 -0400

    adding UI work.

---
+++ UI/src/utils/useRestaurant.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { FETCH_MENU_URL } from "../component/body/config";
+
+const useRestaurant = (id) => {
+  const [restaurant, setrestaurant] = useState(null);
+
+  useEffect(() => {
+    getRestaurantinfo();
+  }, []);
+
+  async function getRestaurantinfo() {
+    const data = await fetch(FETCH_MENU_URL + id);
+    console.log("data");
+    console.log(data);
+    const json = await data.json();
+    console.log("json");
+    console.log(json);
+    setrestaurant(json.data);
+  }
+
+  return restaurant;
+};
+
+export default useRestaurant;
---
+++ UI/src/utils/useallRestaurants.js
@@ -0,0 +1,107 @@
+import { useState, useEffect } from "react";
+import { FETCH_RESTAURANTS_URL } from "../component/body/config";
+
+function useAllRestaurant(setfilteredRestaurants) {
+  const [allRestaurants, setallRestaurants] = useState([]);
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    try {
+      //   const data = await fetch(
+      //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      //   );
+      //   const json = await data.json();
+
+      //   console.log("0000");
+      //   console.log(json);
+      //   console.log(
+      //     json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+      //       ?.restaurants
+      //   );
+      //   console.log(
+      //     json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+      //       ?.restaurants
+      //   );
+      //   setallRestaurants(
+      //     json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+      //       ?.restaurants
+      //   );
+      //   setfilteredRestaurants(
+      //     json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+      //       ?.restaurants
+      //   );
+
+      const data = await fetch("http://localhost:8081/restaurants");
+      const json = await data.json();
+      const mappedData = json.map((item) => ({
+        info: {
+          id: item.id.toString(),
+          name: item.name,
+          cloudinaryImageId: item.restaurant_image,
+          areaName: item.areaName,
+          costForTwo: "₹250 for two",
+          cuisines: [],
+          avgRating: item.avgRating,
+          parentId: "",
+          avgRatingString: item.avgRating.toString(),
+          totalRatingsString: "",
+          sla: {
+            deliveryTime: item.deliveryTime,
+            lastMileTravel: 0,
+            serviceability: "",
+            slaString: "",
+            lastMileTravelString: "",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              imageBased: {},
+              textBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "ITEMS",
+            subHeader: "AT ₹179",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+        },
+      }));
+      console.log("kjdjibjdij");
+      console.log(mappedData);
+      setallRestaurants(mappedData);
+      setfilteredRestaurants(mappedData);
+    } catch (error) {
+      console.error("Error fetching restaurants:", error);
+      // Handle the error here, such as displaying an error message to the user
+    }
+  }
+  return allRestaurants;
+}
+export default useAllRestaurant;
