68a84939aafee79691a07c4abf2e55622f5bb3b3
koundinya <koundinya511@gmail.com>
2024-04-08 17:26:47 -0400

    Added basic backend setup with env file

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,18 @@
+// Accessing Express
 const express = require("express");
 const mysql = require("mysql");
+// To handle cross platform we gona use cors
 const cors = require("cors");
-
+// For environmental variable we are going to use dotenv package
+require("dotenv").config();
 const app = express();
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "food_db",
+  host: process.env.HOSTNAME || "localhost",
+  user: process.env.USER || "root",
+  password: process.env.PASSWORD || "1234",
+  database: process.env.DATABASE || "food_db",
 });
 
 console.log("Connected to MySQL database:", db.config.database);
