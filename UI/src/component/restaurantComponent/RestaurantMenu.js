99075cebdfd4357210f198846e106a3115603a90
ShellyCS <mailstosaily@gmail.com>
2024-04-09 01:53:03 -0400

    fixes

--- UI/src/component/restaurantComponent/RestaurantMenu.js
+++ UI/src/component/restaurantComponent/RestaurantMenu.js
@@ -7,13 +7,10 @@ import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { id } = useParams();
-  console.log("useParams");
-  console.log(useParams());
   const restaurants = useRestaurant(id);
   const [isVisible, setIsVisible] = useState();
 
   if (restaurants == null) return <Shimmer />;
-  console.log("uuu");
   //   console.log(restaurants);
   //   console.log(restaurants.cards);
   const restaurant = restaurants.cards[2].card.card.info;
@@ -25,9 +22,6 @@ const RestaurantMenu = () => {
         singleCategory.card?.card?.["@type"] ==
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
-
-  console.log("categoryyyy");
-  console.log(categories);
 
   return (
     <div className="w-full py-10">
@@ -56,12 +50,7 @@ const RestaurantMenu = () => {
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
             <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-            <a
-              href="#"
-              className="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"
-            >
-              {restaurant?.totalRatings} reviews
-            </a>
+            <span>{restaurant?.totalRatings} reviews</span>
           </div>
           <h2 className="font-semibold m-left">
             <span className="font-normal">City</span> : {restaurant.city}
